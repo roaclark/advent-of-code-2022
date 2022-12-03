095d51fb4c31054a353a34d0cec5e4f74a5a4218
Rachel Rosalia <rachel@rosalia.me>
2022-12-03 02:40:22 -0600

    Set up template

---
+++ template/Solution2.hx
@@ -0,0 +1,17 @@
+package;
+
+class Solution2 {
+	static public function solve(input:String) {
+		return null;
+	}
+
+	static public function solveFile(filename:String = 'input.txt') {
+		var fileContents = sys.io.File.getContent(filename);
+		return solve(fileContents);
+	}
+
+	static public function main() {
+		var solution = solveFile();
+		trace(solution);
+	}
+}
